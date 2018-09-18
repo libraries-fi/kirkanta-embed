12eea1d83446fdea6f899fec1371beddc8a90f80
Samu Juvonen <samu.juvonen@kirjastot.fi>
2018-09-18 14:53:37 +0300

    Fixes

--- src/init.config.js
+++ src/init.config.js
@@ -2,7 +2,7 @@ const BUILD_MODE = process.env.NODE_ENV;
 
 const config = {
   // path: "http://hakemisto.kirjastot.fi.local/widgets/v1",
-  path: BUILD_MODE == "production" ? "/widgets/v1" : "/widgets/dev",
+  path: BUILD_MODE == "production" ? "/embed/v1" : "/embed/dev",
   api: "https://api.kirjastot.fi/v3",
   lang: "fi",
 };
