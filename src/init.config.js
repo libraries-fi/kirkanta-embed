591734673fd526be07131b51c22787620be879cd
Samu Juvonen <samu.juvonen@kirjastot.fi>
2019-02-05 07:49:50 +0200

    Move configuration to a .dist file

--- src/init.config.js
+++ src/init.config.js
@@ -1,21 +1,6 @@
 const BUILD_MODE = process.env.NODE_ENV;
 
-const config = (() => {
-  switch (BUILD_MODE) {
-    case "production":
-      return {
-        path: "https://hakemisto.kirjastot.fi/embed/v1",
-        api: "https://api.kirjastot.fi/v3",
-        lang: "fi",
-      };
+import config from '../config';
 
-    default:
-      return {
-        path: "/embed/dev",
-        api: "https://api.kirjastot.fi/v3",
-        lang: "fi",
-      };
-  }
-})();
 
-export default config;
+export default config(BUILD_MODE);
