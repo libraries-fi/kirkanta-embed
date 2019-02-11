79e90936c47a51b098c830231993497ba7f633dc
Samu Juvonen <samu.juvonen@kirjastot.fi>
2019-02-11 14:00:43 +0200

    Production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,87 +1,87 @@
-const VueLoaderPlugin = require("vue-loader/lib/plugin");
-const autoprefixer = require("autoprefixer");
-const path = require("path");
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const autoprefixer = require('autoprefixer')
+const path = require('path')
 
 module.exports = {
-  mode: "development",
+  mode: 'development',
   entry: {
-    "builder": [
-      "@babel/polyfill",
-      "./src/builder.js"
+    'builder': [
+      '@babel/polyfill',
+      './src/builder.js'
     ],
-    "launcher": [
-      "@babel/polyfill",
-      "./src/launcher.js",
+    'launcher': [
+      '@babel/polyfill',
+      './src/launcher.js'
     ],
-    "display": [
-      "./src/init.widget.js",
-      "./src/display.js"
+    'display': [
+      './src/init.widget.js',
+      './src/display.js'
     ],
-    "schedules": [
-      "./src/init.widget.js",
-      "./src/schedules.js"
+    'schedules': [
+      './src/init.widget.js',
+      './src/schedules.js'
     ],
-    "search": [
-      "./src/init.widget.js",
-      "./src/search.js"
+    'search': [
+      './src/init.widget.js',
+      './src/search.js'
     ],
-    "bootstrap": "./node_modules/bootstrap/dist/css/bootstrap.min.css"
+    'bootstrap': './node_modules/bootstrap/dist/css/bootstrap.min.css'
   },
   output: {
-    path: path.resolve(__dirname, "public/dev"),
-    filename: "[name].js"
+    path: path.resolve(__dirname, 'public/dev'),
+    filename: '[name].js'
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
         use: [
-          "style-loader",
-          "css-loader",
+          'style-loader',
+          'css-loader',
           {
-            loader: "postcss-loader",
+            loader: 'postcss-loader',
             options: {
               plugins: [autoprefixer]
             }
           },
-          "sass-loader"
+          'sass-loader'
         ]
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.vue$/,
-        loader: "vue-loader",
+        loader: 'vue-loader'
       },
       {
         test: /\.yaml$/,
-        use: ["js-yaml-loader"]
+        use: ['js-yaml-loader']
       },
       {
         test: /\.svg$/,
-        use: ["svg-inline-loader"]
+        use: ['svg-inline-loader']
       }
     ]
   },
   plugins: [
-    new VueLoaderPlugin,
-    autoprefixer,
+    new VueLoaderPlugin(),
+    autoprefixer
   ],
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       '@': path.resolve('./src'),
-      "vue$": "vue/dist/vue.esm.js",
-      "vuex$": "vuex/dist/vuex.esm.js",
-      "clipboard": "clipboard/dist/clipboard.min.js",
-      "messages.fi.json": path.resolve("translations/messages.fi.yaml"),
-      "messages.sv.json": path.resolve("translations/messages.sv.yaml"),
-      "messages.en.json": path.resolve("translations/messages.en.yaml"),
-      "builder.fi.json": path.resolve("translations/builder.fi.yaml"),
-      "builder.sv.json": path.resolve("translations/builder.sv.yaml"),
-      "builder.en.json": path.resolve("translations/builder.en.yaml"),
+      'vue$': 'vue/dist/vue.esm.js',
+      'vuex$': 'vuex/dist/vuex.esm.js',
+      'clipboard': 'clipboard/dist/clipboard.min.js',
+      'messages.fi.json': path.resolve('translations/messages.fi.yaml'),
+      'messages.sv.json': path.resolve('translations/messages.sv.yaml'),
+      'messages.en.json': path.resolve('translations/messages.en.yaml'),
+      'builder.fi.json': path.resolve('translations/builder.fi.yaml'),
+      'builder.sv.json': path.resolve('translations/builder.sv.yaml'),
+      'builder.en.json': path.resolve('translations/builder.en.yaml')
     }
   }
-};
+}
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,15 +1,15 @@
-const config = require("./webpack.config.js");
-const merge = require("webpack-merge");
-const path = require("path");
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const config = require('./webpack.config.js')
+const merge = require('webpack-merge')
+const path = require('path')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
-delete config.entry.bootstrap;
+delete config.entry.bootstrap
 
 const productionConfig = merge(config, {
-  mode: "production",
+  mode: 'production',
   output: {
-    path: path.resolve(__dirname, "public"),
-    filename: "v1/[name].js"
+    path: path.resolve(__dirname, 'public'),
+    filename: 'v1/[name].js'
   },
   module: {
     rules: [
@@ -17,25 +17,25 @@ const productionConfig = merge(config, {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
             cacheDirectory: true,
             presets: [
-              ["@babel/preset-env", {
+              ['@babel/preset-env', {
                 modules: false,
                 targets: {
                   ie: 11
                 }
               }]
-            ],
+            ]
             // plugins: ["@babel/plugin-transform-runtime"]
           }
         }
       }
     ]
   },
-  plugins: [new UglifyJsPlugin]
-});
+  plugins: [new UglifyJsPlugin()]
+})
 
 // console.log(Object.keys(productionConfig.entry).map((key) => {
 //   // return merge.strategy({entry: 'prepend'})(productionConfig, {[key]: productionConfig.entry[key]});
@@ -55,4 +55,4 @@ const productionConfig = merge(config, {
 //   return buildConfig;
 // });
 
-module.exports = productionConfig;
+module.exports = productionConfig
